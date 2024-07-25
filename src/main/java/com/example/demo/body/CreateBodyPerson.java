09cb8e7d42fd0b820677c01989467e8de4a4058a
Ariunbold Ulamsaikhan <ariunbold.u@eclinic.local>
2024-07-25 11:43:00 +0800

    getting age from date

--- src/main/java/com/example/demo/body/CreateBodyPerson.java
+++ src/main/java/com/example/demo/body/CreateBodyPerson.java
@@ -1,10 +1,13 @@
 package com.example.demo.body;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 public class CreateBodyPerson {
-    private int id;
     private String lastName;
     private String firstName;
     private int age;
+    private Date birthDate;
     
     public String getLastName() {
         return lastName;
@@ -13,23 +16,38 @@ public class CreateBodyPerson {
         return firstName;
     }
     public int getAge() {
-        return age;
+        return calculateAge(birthDate);
     }
-    public int getId(){
-        return id;
+    public Date getBirthDate() {
+        return birthDate;
     }
 
     
-    public void setId(int id){
-        this.id = id;
-    }
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
     public void setAge(int age) {
         this.age = age;
+    }
+
+
+    private int calculateAge(Date birthDate) {
+        Calendar birth = Calendar.getInstance();
+        birth.setTime(birthDate);
+        Calendar today = Calendar.getInstance();
+        System.out.println("here is brithdate  " + birthDate);
+        int age = today.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+        System.out.println(today);
+        if (today.get(Calendar.DAY_OF_YEAR) < birth.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+        System.out.println("birth day of year" + birth.get(Calendar.DAY_OF_YEAR));
+        return age;
     }
 }
--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -1,10 +1,14 @@
 package com.example.demo.entity;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "person")
@@ -13,42 +17,43 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private String firstName;
     private String lastName;
+    
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
     private int age;
 
     public void setFirstName(String firstName){
         this.firstName = firstName;
     }
-
-    public String getFirstName(){
-        return firstName;
-    }
-    
-
-    public long getId(){
-        return id;
-    }
-
-    
-    public void setId(long id){
-        this.id = id;
+    public void setAge(int age){
+        this.age = age;
     }
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 
+    public Date getBirthDate() {
+        return birthDate;
+    }
     public String getLastName(){
         return lastName;
     }
-
-
-    public void setAge(int age){
-        this.age = age;
+    public String getFirstName(){
+        return firstName;
     }
-
-    public Integer getAge(){
+    public long getId(){
+        return id;
+    }
+    public int getAge(){
         return age;
     }
 }
